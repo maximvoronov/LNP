9556c7372282b5d0ab00409b8796e2600e107049
Maksim <neverhood.booster@gmail.com>
2023-02-22 09:28:12 +0300

    Добавлен интерфейс Coil

--- engine.h
+++ engine.h
@@ -3,13 +3,14 @@
 
 #include <device.h>
 #include <coil.h>
+#include <engineproperty.h>
 #include <QString>
 
-class Engine : public Device, public Coil
+class Engine : public Device, public Coil, public EngineProperty
 {
 public:
     Engine();
-
+    /*Device*/
     void setName(QString name) override;
     QString getName() const override;
     void setBrand(QString brand) override;
@@ -20,7 +21,7 @@ public:
     qreal getHeight() const override;
     void setLength(qreal lenght) override;
     qreal getLength() const override;
-
+    /*Coil*/
     void setCurrent(qreal current) override;
     qreal getCurrent() const override;
     void setVoltage(qreal voltage) override;
@@ -29,14 +30,20 @@ public:
     qreal getWattage() const override;
     void setRessistance(qreal ressistance) override;
     qreal getRessistance() const override;
-
-
+    /*EngineProperty*/
+    void setTorque(qreal torque) override;
+    qreal getTorque() const override;
+    void setInercia(qreal inercia) override;
+    qreal getInercia() const override;
+    void setDiameter(qreal diameter) override;
+    qreal getDiameter() const override;
+    void setShaftSize(qreal shaft_size) override;
+    qreal getShaftSize() const override;
 private:
     QString name, brand;
     qreal width, length, height;
     qreal current, voltage, wattage, ressistance;
-
-
+    qreal torque, inercia, diameter, shaftsize;
 };
 
 #endif // ENGINE_H
