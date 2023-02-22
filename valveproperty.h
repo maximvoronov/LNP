a7f805e0dabe501344308667841e9701225fbd69
Maksim <neverhood.booster@gmail.com>
2023-02-22 09:25:38 +0300

    Добавил форматирование

--- valveproperty.h
+++ valveproperty.h
@@ -6,15 +6,13 @@
 class ValveProperty
 {
 public:
-    enum ValveType{DIRRECT = 235, INDIRRECT };
+    enum ValveType{ DIRRECT = 235, INDIRRECT };
     virtual QString getFitting() const = 0;
     virtual void setFitting(QString fitting) = 0;
     virtual qreal getConsumption() const = 0;
     virtual void setConsumption(qreal consumption) = 0;
     virtual qreal getTemperature() const = 0;
     virtual void setTemperature(qreal temperature) = 0;
-
-
 };
 
 #endif // VALVEPROPERTY_H
