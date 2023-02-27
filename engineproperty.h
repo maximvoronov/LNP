ed86ddb851b6ccea4e6877b3de9bbf1e107e13f1
Maksim <neverhood.booster@gmail.com>
2023-02-27 16:34:23 +0300

    Добавил метод

--- engineproperty.h
+++ engineproperty.h
@@ -5,16 +5,18 @@
 class EngineProperty
 {
 public:
-    enum EngineState{ ON = 500, OFF };
-    enum EngineType{ PNEVMO = 503, HYDRO };
+    enum EngineState{ ON = 500, OFF};
+    enum EngineType{ PNEVMO = 503, HYDRO};
     virtual qreal getShaftSize() const = 0;
     virtual void setShaftSize(qreal shaft_size) = 0;
     virtual qreal getTorque() const = 0;
     virtual void setTorque(qreal torque) = 0;
     virtual void setDiameter(qreal diameter) = 0;
     virtual qreal getDiameter() const = 0;
-    virtual qreal getInercia() const = 0;
+    virtual qreal getInercia() const = 0;    
     virtual void setInercia(qreal inercia) = 0;
+    virtual quint32 getMotoHours() const = 0;
+    virtual void setMotoHours(quint32) = 0;
 };
 
 #endif // ENGINEPROPERTY_H
