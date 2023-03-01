3c6be686ef3e9f7692dbb7a0a2e40cac196fed1b
Maksim <neverhood.booster@gmail.com>
2023-03-01 10:47:00 +0300

    Переименовал интерфейс

---
+++ idevice.h
@@ -0,0 +1,30 @@
+#ifndef IDEVICE_H
+#define IDEVICE_H
+#include <qglobal.h>
+
+class IDevice
+{
+public:
+
+    virtual QString getName()  const = 0;
+    virtual void setName(QString name) = 0;
+
+    virtual QString getBrand() const = 0;
+    virtual void setBrand(QString brand) = 0;
+
+    virtual void setLength(qreal lenght) = 0;
+    virtual qreal getLength() const = 0;
+
+    virtual void setWidth(qreal width) = 0;
+    virtual qreal getWidth() const = 0;
+
+    virtual void setHeight(qreal height) = 0;
+    virtual qreal getHeight() const = 0;
+    virtual void setMass(qreal mass) = 0;
+    virtual qreal getMass() const = 0;
+
+    virtual void setMaterial(QString material) = 0;
+    virtual QString getMaterial() const = 0;
+};
+
+#endif // IDEVICE_H
