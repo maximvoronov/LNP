1975f41ee015a0c81eb58315b7960874bbd82324
Maksim <neverhood.booster@gmail.com>
2023-03-10 10:40:15 +0300

    Объявил класс LNPRWin

--- infowindow.h
+++ infowindow.h
@@ -10,17 +10,27 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QImage>
+#include <QPixmap>
+#include <QLabel>
+#include <QGraphicsColorizeEffect>
+#include <QPropertyAnimation>
+#include <QTransform>
 
+class LNPRWin;
 class InfoWindow : public QWidget
 {
     Q_OBJECT
 public:
     InfoWindow(QWidget *parent = nullptr);
-    void update();
+    ~InfoWindow();
+    //void update();
+    qint32 getCounter() const noexcept;
+    void setCounter(qint32 counter) noexcept;
+    void rotate(QTransform *transform, QPixmap *pixmap);
+    void animateRun(QWidget *widget);
+    bool getDeviceInfoData(QArrayData& arraydata);
 public slots:
     void onClose();
-protected:
-    void paintEvent(QPaintEvent *pevent);
 private:
     QFrame *frame = nullptr;
     QTextEdit *tedit = nullptr;
@@ -28,6 +38,11 @@ private:
     QPushButton *b_close = nullptr;
     QVBoxLayout *vert_lay = nullptr;
     QImage *img = nullptr;
+    QPixmap *pixmap = nullptr;
+    QLabel *lbl_picture = nullptr;
+    QTransform *transform = nullptr;
+    qint32 counter = 0;
+    LNPRWin *lnprwin = nullptr;
 };
 
 #endif // INFOWINDOW_H
